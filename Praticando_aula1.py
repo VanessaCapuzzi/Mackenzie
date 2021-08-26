8e7a417cf626069081d4c74df17117757bdae319
Vanessa Sanches Dias Capuzzi <83616013+VanessaCapuzzi@users.noreply.github.com>
2021-08-26 20:38:56 -0300

    Add files via upload

---
+++ Praticando_aula1.py
@@ -0,0 +1,24 @@
+# Escreva um programa em Python que permita ao usuário digitar dois números inteiros e exibir o resultado para cada uma das seguintes operações: soma, subtração, multiplicação, divisão, divisão truncada, resto e exponenciação.
+
+A = int(input())
+B = int(input())
+C = A+B
+print(C)
+
+C = A-B
+print(C)
+
+C = A*B
+print(C)
+
+C = A/B
+print(C)
+
+C = A//B
+print(C)
+
+C = A % B
+print(C)
+
+C = A**B
+print(C)
